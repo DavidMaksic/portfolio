ea4fa40e59cf0bf71a195ebd998f44108f30c384
DavidMaksic <david.maksic88@gmail.com>
2025-08-01 20:23:37 +0200

    Fix

--- i18n.js
+++ i18n.js
@@ -1,6 +1,4 @@
 import { initReactI18next } from 'react-i18next';
-
-import languageDetector from 'i18next-browser-languagedetector';
 import translationEN from './locales/en.json';
 import translationSR from './locales/sr-cyrl.json';
 import i18n from 'i18next';
@@ -14,19 +12,14 @@ const resources = {
    },
 };
 
-const i18nextLng = localStorage.getItem('i18nextLng');
+i18n.use(initReactI18next).init({
+   resources,
+   lng: 'en',
+   fallbackLng: 'en',
 
-i18n
-   .use(languageDetector)
-   .use(initReactI18next)
-   .init({
-      resources,
-      lng: i18nextLng,
-      fallbackLng: 'en',
-
-      interpolation: {
-         escapeValue: false,
-      },
-   });
+   interpolation: {
+      escapeValue: false,
+   },
+});
 
 export default i18n;
