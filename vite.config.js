592d78642fb81427c2967cf64cdb4a9fa0c5cb9a
DavidMaksic <david.maksic88@gmail.com>
2025-09-12 16:18:56 +0200

    Removed PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,8 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 
 export default defineConfig({
-   plugins: [
-      react(),
-      tailwindcss(),
-      mdx(),
-      VitePWA({
-         registerType: 'autoUpdate',
-         manifest: {
-            name: '>David Maksić • Portfolio',
-            short_name: 'Portfolio',
-            description:
-               'On this portfolio I showcase all of my most imporant projects.',
-            theme_color: '#090909',
-            background_color: '#090909',
-            display: 'standalone',
-            start_url: '/',
-            icons: [
-               {
-                  src: '/logo-mobile-sm.png',
-                  sizes: '192x192',
-                  type: 'image/png',
-               },
-               {
-                  src: '/logo-mobile-lg.png',
-                  sizes: '512x512',
-                  type: 'image/png',
-               },
-            ],
-         },
-      }),
-   ],
+   plugins: [react(), tailwindcss(), mdx()],
 });
